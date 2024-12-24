54630576a9aed4175c47944e33fde492a72423d4
Md Azad Hossain <sokalazad@gmail.com>
2024-12-25 00:45:19 +0100

    added tutorial delete by id api

--- index.js
+++ index.js
@@ -96,6 +96,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/all-tutorials/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await tutorialCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
